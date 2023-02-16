b00c77e39c9f459f94b417ef65e1e286fc2d51a7
lench <lench@LAPTOP-JCUG1REO>
2023-02-16 19:09:39 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        byte a = 16;
+        char b = 'f';
+        sbyte c = -28;
+        short d = -29565;
+        ushort e = 25365;
+        int f = -426867;
+        uint g = 5278657;
+        long h = -54321546834298463;
+        ulong i = 6353473963513216843;
+        Console.Write("Тип byte ");
+        Console.WriteLine(a);
+        Console.Write("Тип char ");
+        Console.WriteLine(b);
+        Console.Write("Тип sbyte ");
+        Console.WriteLine(c);
+        Console.Write("Тип short ");
+        Console.WriteLine(d);
+        Console.Write("Тип ushort ");
+        Console.WriteLine(e);
+        Console.Write("Тип int ");
+        Console.WriteLine(f);
+        Console.Write("Тип uint ");
+        Console.WriteLine(g);
+        Console.Write("Тип long ");
+        Console.WriteLine(h);
+        Console.Write("Тип ulong ");
+        Console.WriteLine(i);
+        Console.ReadKey();
+    }
+}
---
+++ ConsoleApp2/Переменные.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число для проверки на чётность: ");
+        string a = Console.ReadLine();
+        int b = Convert.ToInt32(a);
+        Console.Write("Выберите способ проверки: 1- с помощью логики; 2- через остаток");
+        a = Console.ReadLine();
+        switch (a)
+        {
+            case "1":
+                {
+                    if ((b & 1) == 0)
+                    {
+                        Console.WriteLine("Число чётное");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число нечётное");
+                    }
+                    break;
+                }
+            case "2":
+                {
+                    if ((b % 2) == 0)
+                    {
+                        Console.WriteLine("Число чётное");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число нечётное");
+                    }
+                    break;
+                }
+        }
+        Console.ReadLine();
+    }
+}
+
+
+
+       
